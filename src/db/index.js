f4beeb08be682a125959f136885fbcaf8970fcbf
takkarjatin09 <takkarjatin9@gmail.com>
2024-06-14 00:49:17 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import { DB_Name } from "../constants.js";
+import mongoose from "mongoose";
+
+
+const connectDB=async ()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+
+        console.log(`\n MongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection error", error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import dotenv from "dotenv"
+// import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+// import { DB_Name } from "./constants.js";
+
+// dotenv.config({
+//     path: './env'
+// })
+
+
+
+connectDB()
