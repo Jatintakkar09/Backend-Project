97a9096f63a063f9bfda72a49b7d49413781b6c4
takkarjatin09 <takkarjatin9@gmail.com>
2024-06-15 12:41:11 +0530

    adding files for cors and cookie parser

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true  
+}))
+
+app.use(express.json({
+    limit: "16kb"
+}))// to accept data as JSON a limit has been set for security purposes
+
+app.use(express.urlencoded({
+    extended: true, limit: '16kb'
+}))
+
+app.use(express.static("public"))
+//to store files etc i.e public assets
+
+app.use(cookieParser())
+
+
+export { app }
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-// import dotenv from "dotenv"
-// import mongoose from "mongoose";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import connectDB from "./db/index.js";
-// import { DB_Name } from "./constants.js";
+import { DB_Name } from "./constants.js";
 
-// dotenv.config({
-//     path: './env'
-// })
+dotenv.config({
+    path: './env'
+})
 
+import { app } from "./app.js";
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`Ther error is ${error}`);
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is listening at port :
+             ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB Connection error",error);
+})
