e76d3c11526e585803dba681fe128e4d46c9e29b
takkarjatin09 <takkarjatin9@gmail.com>
2024-06-21 14:22:00 +0530

    adding refresh token and access token methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -18,7 +18,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    email: {
+    fullName: {
       type: String,
       required: true,
       lowercase: true,
@@ -43,8 +43,8 @@ const userSchema = new Schema(
       required: true,
     },
     RefreshToken: {
-        type: String
-    }
+      type: String,
+    },
   },
   {
     timestamps: true,
@@ -54,28 +54,45 @@ const userSchema = new Schema(
 // userSchema.pre("save",()=>{})  we dont give callback in hooks like this not a good practice
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("Password")) return next();
-    this.Password = bcrypt.hash(this.Password,10)  
-    next()
-}) // time lagta hai thats why
+  if (!this.isModified("Password")) return next();
+  this.Password = bcrypt.hash(this.Password, 10);
+  next();
+}); // time lagta hai thats why
 
-// methods creation 
+// methods creation
 
-userSchema.methods.isPasswordCorrect= async function 
-(password){
-  return await bcrypt.compare(password,this.Password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.Password);
+};
 // the above is a way in which the userdata that password is provided
 //is similar to the password that is saved
 
-
-
 // making methods to make access tokens and refresh tokens
+userSchema.methods.generateAccessTokens = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: thus.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
-
-
-
-
-
+userSchema.methods.generateRefreshTokens = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
